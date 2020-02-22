8cc067ae6cb5ed779b7c8228d6bd890bd08b1cdf
guididonato <ddnt@me.com>
2020-02-22 07:23:19 -0300

    Initial commit

---
+++ android/app/src/main/java/falandoserio/frasesdodia/com/br/jokenpo/MainActivity.java
@@ -0,0 +1,13 @@
+package falandoserio.frasesdodia.com.br.jokenpo;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
---
+++ lib/jogo.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Column(
+        children: <Widget>[
+          //text
+          //imagem
+          //text resultado
+          //linha 3 imagens
+
+        ],
+      ),
+
+    );
+  }
+}
